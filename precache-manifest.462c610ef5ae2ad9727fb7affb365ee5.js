0ea59c758871aa87c540817ffe25ae8e7c8b6b84
YanjieHe <heyanjie0@outlook.com>
2018-11-06 09:55:12 -0500

    Updates

--- precache-manifest.462c610ef5ae2ad9727fb7affb365ee5.js
+++ precache-manifest.462c610ef5ae2ad9727fb7affb365ee5.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "517bd639906959d65a84",
+    "revision": "a4c9be1c5baf3a6f429a",
     "url": "/movie-recommender-system-web-app/static/css/main.9cc1a031.chunk.css"
   },
   {
-    "revision": "517bd639906959d65a84",
-    "url": "/movie-recommender-system-web-app/static/js/main.517bd639.chunk.js"
+    "revision": "a4c9be1c5baf3a6f429a",
+    "url": "/movie-recommender-system-web-app/static/js/main.a4c9be1c.chunk.js"
   },
   {
     "revision": "56e84368adb02502ec8c",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/movie-recommender-system-web-app/static/js/runtime~main.70f80fc6.js"
   },
   {
-    "revision": "ce53056f5fd87b6ef8522335e0e5356b",
+    "revision": "25ff13fe8d3832c373a86637e6365c36",
     "url": "/movie-recommender-system-web-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie-recommender-system-web-app/precache-manifest.858bf1e789defb36dd244fee6c7b4447.js"
+  "/movie-recommender-system-web-app/precache-manifest.462c610ef5ae2ad9727fb7affb365ee5.js"
 );
 
 workbox.clientsClaim();
