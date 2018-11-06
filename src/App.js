420972f1b9dc58860389ab586b6fdd5b3402ddea
YanjieHe <heyanjie0@outlook.com>
2018-11-05 22:38:34 -0500

    search bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-        <div>
-            <h1> Hello World! </h1>
-        </div>
+
+    searchMovie(movieName) {
+    }
+
+    render() {
+        return (
+            <div>
+                <h1> Hello World! </h1>
+                <SearchBar searchMovie={this.searchMovie.bind(this)}/>
+            </div>
     );
-  }
 }
+}
+
+class SearchBar extends React.Component {
+    constructor() {
+        this.movieName = "";
+    }
+
+    render({searchMovie}) {
+        return (
+            <div>
+                <input ref={node => {
+                    this.movieName = node;
+                }} />
+                <button onClick={() => {
+                    searchMovie(this.movieName);
+                    this.movieName.value = "";
+                }}>
+                Search
+                </button>
+            </div>
+        );
+    }
+}
+
 
 export default App;
