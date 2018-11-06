72cccfe20af9147e8b6c96a2fa597c806992ce10
YanjieHe <heyanjie0@outlook.com>
2018-11-05 22:03:45 -0500

    Updates

---
+++ precache-manifest.1c428c0c4cf3c591279293dde6b0c6b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "23ae9af616b78e5cc89c",
+    "url": "/movie-recommender-system-web-app/static/css/main.9cc1a031.chunk.css"
+  },
+  {
+    "revision": "23ae9af616b78e5cc89c",
+    "url": "/movie-recommender-system-web-app/static/js/main.23ae9af6.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/movie-recommender-system-web-app/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "70f80fc67e43944285ce",
+    "url": "/movie-recommender-system-web-app/static/js/runtime~main.70f80fc6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie-recommender-system-web-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "489a8c6a768ddab64669f0d45dd2f83f",
+    "url": "/movie-recommender-system-web-app/index.html"
+  }
+];
