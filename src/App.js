14dc9326523365f07478299e146c00eabd59c882
YanjieHe <heyanjie0@outlook.com>
2018-11-06 09:10:51 -0500

    button

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React, { Component } from 'react';
-//import logo from './logo.svg';
-//import './App.css';
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import ReactDOM from "react-dom"
 
 class App extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            data: "Initial data..."
+            data: ""
         }
         this.updateState = this.updateState.bind(this);
+        this.clearInput = this.clearInput.bind(this);
     }
     
     updateState(e) {
         this.setState({data: e.target.value});
     }
 
+    clearInput() {
+        this.setState({data: ""});
+        ReactDOM.findDOMNode(this.refs.myInput).focus();
+    }
+
     render() {
         return (
             <div>
-                <input type = "text" value = {this.state.data}
-                    onChange = {this.updateState} />
-                <h4>{this.state.data}</h4>
+                <input value={this.state.data}
+                    onChange={this.updateState}
+                    ref="myInput">
+                    <button onClick={this.clearInput}>
+                    Clear
+                    </button>
+                </input>
             </div>
         );
     }
