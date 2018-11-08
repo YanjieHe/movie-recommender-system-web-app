74b5b8c3a429be18e23ab1b1d1703dceab0e9055
YanjieHe <heyanjie0@outlook.com>
2018-11-07 23:14:29 -0500

    table

--- src/App.js
+++ src/App.js
@@ -3,37 +3,69 @@
 import './App.css';
 import React from "react";
 import ReactDOM from "react-dom"
+import Movies from "./Movies.js";
 
 class App extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            data: ""
+            data: Movies,
+            search: ""
         }
+
         this.updateState = this.updateState.bind(this);
-        this.clearInput = this.clearInput.bind(this);
+        this.searchMovie = this.searchMovie.bind(this);
     }
     
     updateState(e) {
-        this.setState({data: e.target.value});
+        this.setState({search: e.target.value});
     }
 
-    clearInput() {
-        this.setState({data: ""});
+    searchMovie() {
+        let data = [ ];
+        for(var i = 0; i < Movies.length; i++) {
+            if(Movies[i].includes(this.state.search)) {
+                data.push(Movies[i]);
+            }
+        }
+        alert("You searched movies with keyword: " + this.state.search)
+        this.setState({data: data, search: ""});
         ReactDOM.findDOMNode(this.refs.myInput).focus();
     }
 
     render() {
         return (
             <div>
-                <input value={this.state.data}
-                    onChange={this.updateState}
+                <input value={this.state.search}
+                    onChange = {this.updateState}
                     ref="myInput">
-                    <button onClick={this.clearInput}>
-                    Clear
-                    </button>
                 </input>
+                <button onClick={this.searchMovie}>
+                Search
+                </button>
+                <table>
+                    {this.state.data.map(
+                        (dynamicComponent, i) =>
+                            <Content componentData={dynamicComponent}/>)}
+                </table>
+            </div>
+        );
+    }
+}
+
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <tr>
+                    <td>
+                        <span>
+                        {this.props.componentData}
+                        <br />
+                        </span>
+                    </td>
+                </tr>
             </div>
         );
     }
